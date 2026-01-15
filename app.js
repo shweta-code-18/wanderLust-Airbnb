5ae82bc7785c580ada4ca69da2f476dfe25454b8
shweta-code-18 <shweta38397@gmail.com>
2026-01-15 18:26:43 +0530

    added error handling and validation for Schema

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { nextTick } = require("process");
+const { listingSchema } = require("./schema.js");
+const { valid } = require("joi");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -34,6 +35,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) => {
   res.send("Hello I am building a wonderful website");
 });
+
+const validateListings = (req, res,next) => {
+  let {error} = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }else{
+    next();
+  }
+};
 
 //Index Route
 
@@ -66,10 +77,8 @@ app.get(
 
 app.post(
   "/listings",
+  validateListings,
   wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Please enter valid data for listing");
-    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -90,6 +99,7 @@ app.get(
 
 app.put(
   "/listings/:id",
+  validateListings,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
@@ -127,7 +137,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 505, message = "Something went wrong" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
